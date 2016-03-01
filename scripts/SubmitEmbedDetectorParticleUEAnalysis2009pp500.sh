b626f788c2d11b24fe85070776bce0f2577cb8d2
gdwebb <gdwebb@bnl.gov>
2016-03-01 14:49:11 -0500

    Simple bash script to run the xml over all partonic pT bins.

---
+++ scripts/SubmitEmbedDetectorParticleUEAnalysis2009pp500.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+star-submit-template -template RunEmbedDetectorParticleUEAnalysis.xml -entities ptmin=4,ptmax=5
+star-submit-template -template RunEmbedDetectorParticleUEAnalysis.xml -entities ptmin=5,ptmax=7
+star-submit-template -template RunEmbedDetectorParticleUEAnalysis.xml -entities ptmin=7,ptmax=9
+star-submit-template -template RunEmbedDetectorParticleUEAnalysis.xml -entities ptmin=9,ptmax=11
+star-submit-template -template RunEmbedDetectorParticleUEAnalysis.xml -entities ptmin=11,ptmax=15	
+star-submit-template -template RunEmbedDetectorParticleUEAnalysis.xml -entities ptmin=15,ptmax=25	
+star-submit-template -template RunEmbedDetectorParticleUEAnalysis.xml -entities ptmin=25,ptmax=35	
+star-submit-template -template RunEmbedDetectorParticleUEAnalysis.xml -entities ptmin=35,ptmax=45	
+star-submit-template -template RunEmbedDetectorParticleUEAnalysis.xml -entities ptmin=45,ptmax=55	
+star-submit-template -template RunEmbedDetectorParticleUEAnalysis.xml -entities ptmin=55,ptmax=65	
+star-submit-template -template RunEmbedDetectorParticleUEAnalysis.xml -entities ptmin=65,ptmax=75	
+star-submit-template -template RunEmbedDetectorParticleUEAnalysis.xml -entities ptmin=75,ptmax=-1
